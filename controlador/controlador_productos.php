8bca94606775e7f9c740997deca6895c5f939ed8
Daniel Eduardo Macias Estrada <danma1708@gmail.com>
2023-03-29 23:05:41 -0600

    Update controlador_productos.php

--- controlador/controlador_productos.php
+++ controlador/controlador_productos.php
@@ -34,7 +34,6 @@ if($results==NULL){
     // Se recorre el resultado con todos los registros
     foreach($results as $data => $column){
         echo "<tr>";
-        echo "<td>".$column['codigo']."</td>";
         echo "<td>".$column['nombre']."</td>";
         echo "<td>".$column['descripcion']."</td>";
         echo "<td>".$column['precio_venta']."</td>";
